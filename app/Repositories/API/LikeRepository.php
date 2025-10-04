343e8dd9d33e824a2b532fbfb2ba09f22b07fad4
Adhi Nugraha <midi.adhi@gmail.com>
2025-10-04 09:35:58 +0700

    add missing person images

--- app/Repositories/API/LikeRepository.php
+++ app/Repositories/API/LikeRepository.php
@@ -17,7 +17,7 @@ class LikeRepository extends BaseRepository
     public function getLikedByUserId($request)
     {
         $datas = $this->model->where('user_id', $request->personId)
-            ->where('is_like', true)->with('person');
+            ->where('is_like', true)->with(['person', 'person.images']);
 
         return $datas;
     }
